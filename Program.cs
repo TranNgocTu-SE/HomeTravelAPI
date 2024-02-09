e9a6744f5581ece3fa1ff928f1e92d70a2c6152e
Libera <tranngoctu.se@gmail.com>
2024-02-09 20:05:28 +0700

    update file program

--- Program.cs
+++ Program.cs
@@ -3,12 +3,10 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.ConfigureSwaggerGen(setup =>
+builder.Services.AddSwaggerGen( c =>
 {
-    setup.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
     {
         Title = "Weather Forecasts",
         Version = "v1"
