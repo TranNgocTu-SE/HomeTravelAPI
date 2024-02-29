408a1143340a7e66f4459387a219a2c2428ddd05
khoinmse161104@fpt.edu.vn <158076415+minhkhoi2706@users.noreply.github.com>
2024-02-29 07:44:21 +0700

    fixSearch

--- Services/LocationService.cs
+++ Services/LocationService.cs
@@ -24,12 +24,20 @@ namespace HomeTravelAPI.Services
             var filter = _mapper.Map<LocationRequestViewModel, LocationViewModel>(locationRequestViewModel);
             filter.SortDirection = pagingViewModel.SortDirection;
             filter.SortProperty = pagingViewModel.SortProperty;
+            IQueryable<Location> query;
 
-            var query = _context.Locations.Include(l => l.HomeStay).AsQueryable();
+            if(locationRequestViewModel.LocationField != null)
+            {
+                query = _context.Locations.Include(l => l.HomeStay).Where(x => x.CityName.Equals(locationRequestViewModel.LocationField)
+         || x.DistrictName.Equals(locationRequestViewModel.LocationField)
+         || x.ProvinceName.Equals(locationRequestViewModel.LocationField));
+            }
+
+            else query = _context.Locations.Include(l => l.HomeStay);
 
             query = query.Where(l => l.HomeStay.TotalCapacity >= locationRequestViewModel.TotalTourist);
 
-            var response = await query.ProjectTo<LocationViewModel>(_mapper.ConfigurationProvider).DynamicFilter(filter).DynamicSort(filter).PagingQueryable(pagingViewModel.Page, pagingViewModel.PageSize).Item2.ToListAsync();
+            var response = await query.ProjectTo<LocationViewModel>(_mapper.ConfigurationProvider).DynamicSort(filter).PagingQueryable(pagingViewModel.Page, pagingViewModel.PageSize).Item2.ToListAsync();
 
             return response;
         }
--- ViewModels/LocationRequestViewModel.cs
+++ ViewModels/LocationRequestViewModel.cs
@@ -4,9 +4,7 @@ namespace HomeTravelAPI.ViewModels
 {
     public class LocationRequestViewModel
     {
-        public string? CityName { get; set; }
-        public string? DistrictName { get; set; }
-        public string? ProvinceName { get; set; }
+        public string? LocationField { get; set; }
         public int TotalTourist { get; set; }
     }
 }
